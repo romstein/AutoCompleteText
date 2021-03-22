5a8f6b78af979b03ad371b94bcd4bc3532f0e000
Goryunov <Goryunov@PC403-02>
2021-03-22 14:50:42 +0300

    Add Dictionary

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -8,7 +8,35 @@ namespace TextAnalysis
         {
             var result = new Dictionary<string, string>();
             HashSet<string> set = new HashSet<string>();
-            //...
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i=0;i<text.Count;i++)
+            {
+                for (int j=0;j<text[i].Count-2;j++)
+                {
+                    string s = text[i][j] + " " + text[i][j + 1];
+                    if (text[i].Count > 2)
+                    {
+                        string s1 = text[i][j] + " " + text[i][j + 1] + " " + text[i][j + 2];
+                        if (!dict.ContainsKey(s))
+                        {
+                            dict.Add(s, 1);
+                        }
+                        else
+                        {
+                            dict[s]++;
+                        }
+                        if (!dict.ContainsKey(s1))
+                        {
+                            dict.Add(s1, 1);
+                        }
+                        else
+                        {
+                            dict[s1]++;
+                        }
+                    }
+                }
+            }
+
             return result;
         }
    }
