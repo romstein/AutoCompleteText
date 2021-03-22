8aa892104c0b9d4c58589a22f7b2d39c461fe0bf
Goryunov <Goryunov@PC403-02>
2021-03-22 13:59:18 +0300

    Add HashSet

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -7,6 +7,7 @@ namespace TextAnalysis
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
+            HashSet<string> set = new HashSet<string>();
             //...
             return result;
         }
